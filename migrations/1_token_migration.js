bb6fdd5e38d36202e3f981d47d169596a75d1812
jupiteruwdev <harutoinoue8@gmail.com>
2021-07-01 20:15:11 +0530

    testing changes

--- migrations/1_token_migration.js
+++ migrations/1_token_migration.js
@@ -4,13 +4,4 @@ const Enoch = artifacts.require("Enoch");
 module.exports = async function (deployer, networks, accounts) {
   await deployer.deploy(FinAxis);
   await deployer.deploy(Enoch);
-  
-  const finaInstance = await FinAxis.deployed();
-  const EnochInstance = await Enoch.deployed();
-  
-  const fina_balance = await finaInstance.balanceOf(finaInstance.address);
-  const Enoch_balance = await EnochInstance.balanceOf(EnochInstance.address);
-
-  console.log('intial FINA contract balance: ', fina_balance.toString());
-  console.log('intial Enoch contract balance: ', Enoch_balance.toString());
 };
--- test/FinAxis.js
+++ test/FinAxis.js
@@ -7,4 +7,16 @@ contract("FinAxis", accounts => {
         
         assert.equal(deployer_balance.toString(), '210000000000000000');
     })
+
+    it('should tranfer 100 FINA from deployer address to another address', async () => {
+        let finaInstance = await FinAxis.deployed();
+        const tx = await finaInstance.transfer(accounts[1], 1000000000000);
+
+        const balance0 = await finaInstance.balanceOf(accounts[0]);
+        console.log("FINA balance of account 0: ", balance0.toString());
+
+        const balance1 = await finaInstance.balanceOf(accounts[1]);
+        console.log("FINA balance of account 1: ", balance1.toString())
+
+    })
 })
